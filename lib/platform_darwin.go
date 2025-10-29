18f09d8a73726dc4cf021f42659090dfa179889a
Peter Romianowski <pero@cling.com>
2025-10-29 15:14:43 +0100

    ...

--- lib/platform_darwin.go
+++ lib/platform_darwin.go
@@ -29,7 +29,7 @@ func EnhancedStat(fileInfo fs.FileInfo) (*EnhancedStat_t, error) {
 	}
 	return &EnhancedStat_t{
 		CTimeSec:  stat.Ctimespec.Sec,
-		CTimeNSec: int32(stat.Ctimespec.Nsec),
+		CTimeNSec: int32(stat.Ctimespec.Nsec), //nolint:gosec
 		Inode:     stat.Ino,
 	}, nil
 }
